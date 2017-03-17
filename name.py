ef9fe753fd7f0cccbdd41726083552baf72665eb
Ayush <aayushban100@gmail.com>
2017-03-17 13:09:54 +0545

    quadradic fuction

--- name.py
+++ name.py
@@ -2,5 +2,5 @@ first_name= "Ayush"
 last_name= "Ban"
 home_town ="Kathmandu, Nepal"
 
-print("Hi! I am", first_name, last_name, ".")
+print("Hi! I am", first_name, last_name,".")
 print("I'm from", home_town,".")
---
+++ quadratic.py
@@ -0,0 +1,11 @@
+a= 10
+b=99
+c= 10
+d= ( b ** 2 - 4 * a * c) ** .5
+
+x1= (-b + d) / (2 * a)
+
+x2= (-b - d)  / (2 * a)
+
+print("The two values of x are ", x1 , x2)
+
