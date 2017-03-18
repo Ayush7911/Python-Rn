6890fdcaa28e7d42ed5b719556e434252b88cf68
Ayush <aayushban100@gmail.com>
2017-03-18 08:48:01 +0545

    first string example

---
+++ string.py
@@ -0,0 +1,9 @@
+s= input('Enter a string')
+
+print("You have entered" +s)
+print("No. of characters = %d " % len(s))
+print("First character = %s " %s[0])
+print("Last character = %s " %[len(s)-1])
+
+
+
