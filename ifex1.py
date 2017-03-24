8efab4ceb196e6de80383a55f29c8ea8e2a2529e
Ayush <aayushban100@gmail.com>
2017-03-24 10:47:53 +0545

    first ifelse exercise

---
+++ ifex1.py
@@ -0,0 +1,25 @@
+age=int(input('Age: '))
+
+if age<=0:
+    print('Invalid input for age')
+
+elif age<=1:
+    print('Youre an infant')
+
+elif age<=12:
+    print('Youre a kid')
+
+elif age<=19:
+    print('You are teenager')
+
+elif age<=45:
+    print('you are a adult')
+
+elif age<=59:
+    print('you are middle-aged')
+
+elif age<=60:
+    print('tou are old')
+
+elif age<=120:
+    print('You are way too old')
