cdcada60d3b84fcf755c0bfd33a56db4cd7453ad
Ayush <aayushban100@gmail.com>
2017-03-24 10:16:43 +0545

    multiple ifelse sentence

---
+++ multiple-ifelse.py
@@ -0,0 +1,19 @@
+words= input('words:')
+if words  =='Foo':
+    print('Great!')
+    print('You entered "Foo"')
+
+elif words == 'Bar':
+    print('Wow')
+    print('You entered "Bar"')
+
+elif words == 'Baz':
+    print('Awesome!')
+    print('You entered "Baz"')
+
+else:
+    print('Hey!')
+    print('You entered wronged')
+
+print('Good Bye!')
+
