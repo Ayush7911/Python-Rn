4f6ab8e5c722d43ff85938053ea46feaf93b95dc
Ayush <aayushban100@gmail.com>
2017-03-25 09:28:33 +0545

    loop over dictionary or list of dictionary

---
+++ dictitems.py
@@ -0,0 +1,9 @@
+user={
+    'name':'John Doe',
+    'email':'Johndoe@example.com',
+    'phone':'(111)111-11112',
+    'address':'123 6th kathmandu'
+}
+
+for (key,val) in user.items():
+    print('%s:%s'%(key,val))
